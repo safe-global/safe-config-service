ad520f9eeaaa9f820637e68384b5a8df0f6d2184
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-19 18:07:12 +0200

    Add namespaces. Add pytest setup

---
+++ safe_client_config_service/apps/tests/test_views.py
@@ -0,0 +1,15 @@
+import json
+
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from ..models import SafeApp
+
+
+class SafeAppsListViewTests(APITestCase):
+    def test_get_queryset(self):
+        url = reverse('v1:safe-apps')
+        response = self.client.get(path=url, data=None, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(SafeApp.objects.count(), 0)
+        self.assertEqual(json.loads(response.content), [])
--- safe_client_config_service/safe_client_config_service/urls.py
+++ safe_client_config_service/safe_client_config_service/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1/', include('apps.urls')),
+    path('api/v1/', include('apps.urls', namespace='v1')),
     path('admin/', admin.site.urls),
 ]
