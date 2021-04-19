598b584beb8aef0438d1de1c648d8cfb5d4e7f68
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-19 22:37:57 +0200

    Add test Github action

--- safe_client_config_service/apps/tests/test_views.py
+++ safe_client_config_service/apps/tests/test_views.py
@@ -7,7 +7,7 @@ from ..models import SafeApp
 
 
 class SafeAppsListViewTests(APITestCase):
-    def test_get_queryset(self):
+    def test_empty_set(self):
         url = reverse('v1:safe-apps')
         response = self.client.get(path=url, data=None, format='json')
         self.assertEqual(response.status_code, 200)
