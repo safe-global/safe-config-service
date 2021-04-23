055a7e51738e7e2644176498a968fda78954df30
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-23 12:00:04 +0200

    Use correct host when running in a container

--- src/config/settings.py
+++ src/config/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
