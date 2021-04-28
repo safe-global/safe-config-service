c3d4b772b26740743eb39415c2e3ebad0867c4d0
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-28 21:02:01 +0200

    Add initial default apps fixture (#6)
    - Adds a default initial list for the /api/v1/safe-apps/ endpoint. The list was taken from gnosis/safe-apps-list
    - The list can be loaded to the database via python src/manage.py loaddata initial_default_apps.json
      * It does not delete already existing data
      * It updates entries that match the url (primary key) – ie.: the value of this fixture overrides the current entry already set in the database (if any)
    - Primary key is now the application URL (removed the auto-generated primary key)

--- src/safe_apps/models.py
+++ src/safe_apps/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class SafeApp(models.Model):
-    url = models.URLField()
+    url = models.URLField(primary_key=True)
     name = models.CharField(max_length=200)
     icon_url = models.URLField()
     description = models.CharField(max_length=200)
