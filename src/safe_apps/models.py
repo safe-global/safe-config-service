1c1e210d2be5f2880cb4ef18a67b0ac6c1b82755
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-29 15:10:03 +0200

    Add Provider info. Convert fixture to migration

--- src/safe_apps/models.py
+++ src/safe_apps/models.py
@@ -2,9 +2,21 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
+class Provider(models.Model):
+    url = models.URLField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name} | {self.url}'
+
+
 class SafeApp(models.Model):
     url = models.URLField(primary_key=True)
     name = models.CharField(max_length=200)
     icon_url = models.URLField()
     description = models.CharField(max_length=200)
     networks = ArrayField(models.IntegerField())
+    provider = models.ForeignKey(Provider, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.name} | {self.url} | networks={self.networks}'
