7026da9fa2fe34bdc97cc9df77a4ba8854d3f3af
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-29 18:03:30 +0200

    Serializer Provider Info

--- src/safe_apps/serializers.py
+++ src/safe_apps/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import SafeApp
+from .models import SafeApp, Provider
+
+
+class ProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ['url', 'name']
 
 
 class SafeAppsResponseSerializer(serializers.ModelSerializer):
+    provider = ProviderSerializer()
+
     class Meta:
         model = SafeApp
-        fields = ['url', 'name', 'icon_url', 'description', 'networks']
+        fields = ['url', 'name', 'icon_url', 'description', 'networks', 'provider']
