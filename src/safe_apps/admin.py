d78c63893ef7ee478234c800ff8d423e64a0fe8a
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-29 17:02:21 +0200

    Add Safe App Provider info. Update fixture (#8)
    - Adds a new table for Safe App Providers – Provider. The Provider schema currently has a name and a url
    - Adds __str__ methods to current models.

--- src/safe_apps/admin.py
+++ src/safe_apps/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import SafeApp
+from .models import SafeApp, Provider
 
-admin.site.register(SafeApp)
+models = [SafeApp, Provider]
+admin.site.register(models)
