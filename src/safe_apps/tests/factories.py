d6e727903101702e456f029a82332a6ee9267bef
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-04-30 15:48:46 +0200

    Use FactoryBoy for generating test data

---
+++ src/safe_apps/tests/factories.py
@@ -0,0 +1,24 @@
+import factory
+from factory.django import DjangoModelFactory
+
+from ..models import Provider, SafeApp
+
+
+class ProviderFactory(DjangoModelFactory):
+    class Meta:
+        model = Provider
+
+    name = factory.Faker('company')
+    url = factory.Faker('url')
+
+
+class SafeAppFactory(DjangoModelFactory):
+    class Meta:
+        model = SafeApp
+
+    url = factory.Faker('url')
+    name = factory.Faker('company')
+    icon_url = factory.Faker('image_url')
+    description = factory.Faker('catch_phrase')
+    networks = factory.Faker('pylist', nb_elements=2, value_types=(int,))
+    provider = None
