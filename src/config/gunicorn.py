d6604c7211bb9bca36c5fdfb598f880466d4f1d4
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-04 15:40:27 +0200

    Use Nginx templates to replace environment variables. Extract port variables

--- src/config/gunicorn.py
+++ src/config/gunicorn.py
@@ -2,7 +2,7 @@ import multiprocessing
 import os
 from distutils.util import strtobool
 
-bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
+bind = f"0.0.0.0:{os.getenv('GUNICORN_BIND_PORT', '8000')}"
 accesslog = "-"
 
 workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
