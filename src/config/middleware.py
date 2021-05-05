a7170d7a4b637b655938dc8e9c116e570412133c
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-05 16:41:10 +0200

    Add Logging middleware

---
+++ src/config/middleware.py
@@ -0,0 +1,37 @@
+import logging
+import time
+
+from django.http import HttpRequest
+
+
+class LoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.logger = logging.getLogger("LoggingMiddleware")
+
+    @staticmethod
+    def get_milliseconds_now():
+        return int(time.time() * 1000)
+
+    def __call__(self, request: HttpRequest):
+        # before view (and other middleware) are called
+        milliseconds = self.get_milliseconds_now()
+
+        response = self.get_response(request)
+
+        # after view is called
+        if request.resolver_match:
+            route = (
+                request.resolver_match.route[1:]
+                if request.resolver_match
+                else request.path
+            )
+            self.logger.info(
+                "MT::%s::%s::%s::%d::%s",
+                request.method,
+                route,
+                self.get_milliseconds_now() - milliseconds,
+                response.status_code,
+                request.path,
+            )
+        return response
