c408da3908177304ea5601698e067db689129e32
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-14 18:19:31 +0200

    Use Unix socket in addition to TCP to bind application (#60)
    - Use Unix socket to bind the application
    - This avoids Nginx requiring to know the name registered of the container/service which is running the Gunicorn app

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ echo "==> $(date +%H:%M:%S) ==> Migrating Django models..."
 python src/manage.py migrate --noinput
 
 echo "==> $(date +%H:%M:%S) ==> Running Gunicorn..."
-exec gunicorn -c /app/src/config/gunicorn.py config.wsgi -b 0.0.0.0:${GUNICORN_BIND_PORT} --chdir /app/src/
+exec gunicorn -c /app/src/config/gunicorn.py config.wsgi -b ${GUNICORN_BIND_SOCKET} -b 0.0.0.0:${GUNICORN_BIND_PORT} --chdir /app/src/
