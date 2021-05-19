3a2afb22de8d5a24d42af0e29d98dca4a97daa06
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-19 17:45:51 +0200

    Add /check endpoint (#68)
    Adds /check endpoint that can be used to check the readiness for the service to receive requests

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 urlpatterns = [
     path("api/v1/", include("safe_apps.urls", namespace="v1")),
     path("admin/", admin.site.urls),
+    path("check/", lambda request: HttpResponse("Ok"), name="check"),
 ]
