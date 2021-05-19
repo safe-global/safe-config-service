95457a499577557e7396dd2dd01dbdfc67b8e620
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-19 16:38:34 +0200

    Add /check endpoint

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.http import HttpResponse
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("api/v1/", include("safe_apps.urls", namespace="v1")),
     path("admin/", admin.site.urls),
+    re_path(r"^check/", lambda request: HttpResponse("Ok"), name="check"),
 ]
