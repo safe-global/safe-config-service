60199f367afb0b6d27bc656ed1147e1b2e362cc2
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-19 16:57:38 +0200

    Remove regular expression path

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 urlpatterns = [
     path("api/v1/", include("safe_apps.urls", namespace="v1")),
     path("admin/", admin.site.urls),
-    re_path(r"^check/", lambda request: HttpResponse("Ok"), name="check"),
+    path("check/", lambda request: HttpResponse("Ok"), name="check"),
 ]
