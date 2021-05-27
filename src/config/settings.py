64c7f507454db7d142eae5f8838b1172d3979482
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-05-27 09:48:28 +0200

    Serve static files with Nginx (#76)
    - Collect static files when the web image is deployed with `manage.py collectstatic`
    - Copy collected files folder (`staticfiles/`) to `${DOCKER_NGINX_VOLUME_ROOT}`

--- src/config/settings.py
+++ src/config/settings.py
@@ -171,6 +171,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATIC_ROOT = "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
