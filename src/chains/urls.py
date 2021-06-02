9f925ac9af5cbf408ad12498903b9f87965a190c
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-06-02 18:23:51 +0200

    Add /chains/<chain-id> endpoint

--- src/chains/urls.py
+++ src/chains/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from chains.views import ChainsListView
+from chains.views import ChainsDetailView, ChainsListView
 
 app_name = "chains"
 
 urlpatterns = [
     path("chains/", ChainsListView.as_view(), name="list"),
+    path("chains/<pk>", ChainsDetailView.as_view(), name="detail"),
 ]
