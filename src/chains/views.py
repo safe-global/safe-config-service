70b645dd3eb1a078d0e5c99b4e608221df3934fd
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-06-02 17:34:30 +0200

    Remove unecessary swagger annotation

--- src/chains/views.py
+++ src/chains/views.py
@@ -1,4 +1,3 @@
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import LimitOffsetPagination
 
@@ -11,10 +10,6 @@ class ChainsListView(ListAPIView):
     pagination_class = LimitOffsetPagination
     pagination_class.max_limit = 10
     pagination_class.default_limit = 10
-
-    @swagger_auto_schema()
-    def get(self, request, *args, **kwargs):
-        return super().get(self, request, *args, **kwargs)
 
     def get_queryset(self):
         return Chain.objects.all()
