07c7066aa8c661019db1419e216d1a5d41cd6ae6
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-06-17 17:40:22 +0200

    Add theme information to /chain (#112)
    - Add two new fields to /chains: textColor and backgroundColor
    - Both colours should be represented as an hexadecimal color (support for alpha bits is also present)
    - These fields are stored as a CharField in the database. However in order to save them the following validations are done:
      * It should match the following regex: ^#(?:[0-9a-fA-F]{3,4}){1,2}$
      * It should be maximum 9 characters – even though this is already covered by the regex expressions this is still required by the CharField

--- src/chains/models.py
+++ src/chains/models.py
@@ -1,4 +1,11 @@
+import re
+
+from django.core.validators import RegexValidator
 from django.db import models
+
+HEX_ARGB_REGEX = re.compile("^#(?:[0-9a-fA-F]{3,4}){1,2}$")
+
+color_validator = RegexValidator(HEX_ARGB_REGEX, "Invalid hex color", "invalid")
 
 
 class Chain(models.Model):
@@ -10,6 +17,12 @@ class Chain(models.Model):
     currency_symbol = models.CharField(max_length=255)
     currency_decimals = models.IntegerField(default=18)
     transaction_service_url = models.URLField(null=True)
+    theme_text_color = models.CharField(
+        validators=[color_validator], max_length=9, default="#fff"
+    )
+    theme_background_color = models.CharField(
+        validators=[color_validator], max_length=9, default="#000"
+    )
 
     def __str__(self):
         return f"{self.name} | chain_id={self.id}"
--- src/chains/tests/test_models.py
+++ src/chains/tests/test_models.py
@@ -1,4 +1,7 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.db import DataError
+from django.test import TestCase, TransactionTestCase
+from faker import Faker
 
 from .factories import ChainFactory
 
@@ -10,3 +13,28 @@ class ChainTestCase(TestCase):
             str(chain),
             f"{chain.name} | chain_id={chain.id}",
         )
+
+
+class ChainColorValidationTestCase(TransactionTestCase):
+    def test_invalid_text_colors(self):
+        param_list = ["aaa", "bbb", "#fffffffff", "zzz", "010", "", "a word", "#hhh"]
+        for invalid_color in param_list:
+            with self.subTest(msg=f"Invalid color {invalid_color} should throw"):
+                with self.assertRaises(
+                    (
+                        ValidationError,
+                        DataError,
+                    )
+                ):
+                    chain = ChainFactory.create(theme_text_color=invalid_color)
+                    # run validators
+                    chain.full_clean()
+
+    def test_valid_text_colors(self):
+        param_list = ["#000", "#fff", "#00000000", "#ffffffff"] + [
+            Faker().hex_color() for _ in range(20)
+        ]
+        for valid_color in param_list:
+            with self.subTest(msg=f"Valid color {valid_color} should not throw"):
+                chain = ChainFactory.create(theme_text_color=valid_color)
+                chain.full_clean()
