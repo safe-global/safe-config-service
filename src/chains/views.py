3cda8a63ede20112a1eb475cbbd703d7f46204cb
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-07-08 15:22:28 +0200

    Increase Chains pagination limit (#145)
    Increases /chains pagination max limit to 100

--- src/chains/views.py
+++ src/chains/views.py
@@ -9,7 +9,7 @@ from .serializers import ChainSerializer
 class ChainsListView(ListAPIView):
     serializer_class = ChainSerializer
     pagination_class = LimitOffsetPagination
-    pagination_class.max_limit = 10
+    pagination_class.max_limit = 100
     pagination_class.default_limit = 10
     queryset = Chain.objects.all()
     filter_backends = [filters.OrderingFilter]
