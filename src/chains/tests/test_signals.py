22bd3e67badc8642a6597e9f22808d0d5d531e1b
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2021-11-09 17:03:43 +0100

    Trigger CGW hook on Wallet and Feature update (#299)
    Trigger the CGW /flush endpoint when a Feature or Wallet is created, updated or deleted

--- src/chains/tests/test_signals.py
+++ src/chains/tests/test_signals.py
@@ -1,6 +1,7 @@
 import responses
 from django.test import TestCase, override_settings
 
+from chains.models import Feature, Wallet
 from chains.tests.factories import ChainFactory
 
 
@@ -91,3 +92,91 @@ class ChainNetworkHookTestCase(TestCase):
         ChainFactory.create()
 
         assert len(responses.calls) == 0
+
+
+@override_settings(
+    CGW_URL="http://127.0.0.1",
+    CGW_FLUSH_TOKEN="example-token",
+)
+class FeatureHookTestCase(TestCase):
+    @responses.activate
+    def test_on_feature_create_hook_call(self) -> None:
+        responses.add(
+            responses.POST,
+            "http://127.0.0.1/v1/flush/example-token",
+            status=200,
+            match=[responses.matchers.json_params_matcher({"invalidate": "Chains"})],  # type: ignore[no-untyped-call]
+        )
+
+        Feature(key="Test Feature").save()
+
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.body == b'{"invalidate": "Chains"}'
+        assert (
+            responses.calls[0].request.url == "http://127.0.0.1/v1/flush/example-token"
+        )
+
+    @responses.activate
+    def test_on_feature_delete_hook_call(self) -> None:
+        feature = Feature(key="Test Feature")
+
+        feature.save()  # create
+        feature.delete()  # delete
+
+        # 2 calls: one for creation and one for deletion
+        assert len(responses.calls) == 2
+
+    @responses.activate
+    def test_on_feature_update_hook_call(self) -> None:
+        feature = Feature(key="Test Feature")
+
+        feature.save()  # create
+        feature.key = "New Test Feature"
+        feature.save()  # update
+
+        # 2 calls: one for creation and one for updating
+        assert len(responses.calls) == 2
+
+
+@override_settings(
+    CGW_URL="http://127.0.0.1",
+    CGW_FLUSH_TOKEN="example-token",
+)
+class WalletHookTestCase(TestCase):
+    @responses.activate
+    def test_on_wallet_create_hook_call(self) -> None:
+        responses.add(
+            responses.POST,
+            "http://127.0.0.1/v1/flush/example-token",
+            status=200,
+            match=[responses.matchers.json_params_matcher({"invalidate": "Chains"})],  # type: ignore[no-untyped-call]
+        )
+
+        Wallet(name="Test Wallet").save()
+
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.body == b'{"invalidate": "Chains"}'
+        assert (
+            responses.calls[0].request.url == "http://127.0.0.1/v1/flush/example-token"
+        )
+
+    @responses.activate
+    def test_on_wallet_delete_hook_call(self) -> None:
+        wallet = Wallet(name="Test Wallet")
+
+        wallet.save()  # create
+        wallet.delete()  # delete
+
+        # 2 calls: one for creation and one for deletion
+        assert len(responses.calls) == 2
+
+    @responses.activate
+    def test_on_wallet_update_hook_call(self) -> None:
+        wallet = Wallet(name="Test Wallet")
+
+        wallet.save()  # create
+        wallet.name = "Test Wallet v2"
+        wallet.save()  # update
+
+        # 2 calls: one for creation and one for updating
+        assert len(responses.calls) == 2
