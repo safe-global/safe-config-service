88ade643aa1f67c6b92ecbacf0ec84e304ef9a5a
Uxio Fuentefria <uxio@safe.global>
2022-09-16 12:32:45 +0200

    Customize swagger with Safe logo
    - Add configuration for templates and static files
    - Add collect static to Dockerfile
    - Don't rebuild docker image unless dependencies change

--- src/config/settings.py
+++ src/config/settings.py
@@ -118,7 +118,9 @@ FORCE_SCRIPT_NAME = os.getenv("FORCE_SCRIPT_NAME", default=None)
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            "templates/",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -178,9 +180,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = "static/"
 STATIC_ROOT = "staticfiles"
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
