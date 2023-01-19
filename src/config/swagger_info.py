5e0a95a48afb69a0a2a077b7fc6cd818840ba266
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2023-01-19 14:43:32 +0100

    Update Swagger title (#769)
    - Remove "Gnosis" from the Swagger title

--- src/config/swagger_info.py
+++ src/config/swagger_info.py
@@ -1,7 +1,7 @@
 from drf_yasg import openapi
 
 SAFE_CONFIG_SERVICE_SWAGGER_INFO = openapi.Info(
-    title="Gnosis Safe Config Service API",
+    title="Safe Config Service API",
     default_version="v1",
     description="Service that provides configuration information in the context of the Safe clients environment",
     contact=openapi.Contact(email="safe@gnosis.io"),
