a87d97208e607637f6aaca7ebd6f33acaef6e730
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-06-16 11:21:55 +0100

    Remove contact field from Swagger
    Removes the contact field from the Swagger interface

--- src/config/swagger_info.py
+++ src/config/swagger_info.py
@@ -4,6 +4,5 @@ SAFE_CONFIG_SERVICE_SWAGGER_INFO = openapi.Info(
     title="Safe Config Service API",
     default_version="v1",
     description="Service that provides configuration information in the context of the Safe clients environment",
-    contact=openapi.Contact(email="safe@gnosis.io"),
     license=openapi.License(name="MIT License"),
 )
