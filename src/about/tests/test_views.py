b0eb73e08452408003a2a671fcd1cf8c6f875435
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2023-06-27 13:00:38 +0200

    Add swagger UI view tests

--- src/about/tests/test_views.py
+++ src/about/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 
@@ -46,3 +47,15 @@ class AboutSecureRequestViewTests(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, expected_json_response)
+
+
+class SwaggerTests(APITestCase):
+    def test_swagger_json_schema(self):
+        url = reverse("schema-json", args=(".json",))
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_swagger_ui(self):
+        url = reverse("schema-swagger-ui")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
