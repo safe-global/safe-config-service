45817963d06c4a65955b80caa7c586addc52c75f
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2023-09-08 13:25:45 +0100

    Set timeout when connecting to the CGW (#945)
    - Sets a timeout when connecting to the Safe Client Gateway.
    - The timeout is configurable with the environment variable `CGW_SESSION_TIMEOUT_SECONDS`. If it's not set, 2 seconds will be used.
    - The maximum number of retries is now configurable under `CGW_SESSION_MAX_RETRIES`. If it's not set, no retries will be made (0).

--- src/clients/safe_client_gateway.py
+++ src/clients/safe_client_gateway.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, Optional
 from urllib.parse import urljoin
 
 import requests
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,9 @@ logger = logging.getLogger(__name__)
 @cache
 def setup_session() -> requests.Session:
     session = requests.Session()
-    adapter = requests.adapters.HTTPAdapter(max_retries=3)
+    adapter = requests.adapters.HTTPAdapter(
+        max_retries=settings.CGW_SESSION_MAX_RETRIES
+    )
     session.mount("http://", adapter)
     session.mount("https://", adapter)
     return session
@@ -33,6 +36,7 @@ def flush(
             url,
             json=json,
             headers={"Authorization": f"Basic {cgw_flush_token}"},
+            timeout=settings.CGW_SESSION_TIMEOUT_SECONDS,
         )
         post.raise_for_status()
     except Exception as error:
