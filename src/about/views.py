fbb334b37c97dfd9d1ffc066b385a6fb63f348ad
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2024-04-01 22:14:18 +0100

    Add Version and Builder Number to Docker image (#1085)
    - Adds the following build arguments:
      * `VERSION` – represents the version of the application (e.g.: semver). It is accessible across the app via `settings.APPLICATION_VERSION`.
      * `BUILD_NUMBER` – represents the build version of the application (e.g.: build hash). It is accessible across the app via `settings.APPLICATION_BUILD_NUMBER`
    - With the added support for the build arguments, the version of the application can now be set during the docker image building process instead of bundling the version with the application.

--- src/about/views.py
+++ src/about/views.py
@@ -1,14 +1,15 @@
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from version import __name__, __version__
+from version import __name__
 
 
 class AboutView(APIView):
     def get(self, request, format=None):
         response = {
             "name": __name__,
-            "version": __version__,
+            "version": settings.APPLICATION_VERSION,
             "api_version": self.request.version,
             "secure": self.request.is_secure(),
         }
