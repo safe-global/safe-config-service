7901990a45aac094eb74ab82ca56ef18c0f581b7
Pooya Raki <pooya.raki@gmail.com>
2025-12-09 11:04:50 +0100

    fix: revert database port

--- src/config/settings.py
+++ src/config/settings.py
@@ -149,7 +149,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": os.getenv("POSTGRES_PORT", "5433"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
